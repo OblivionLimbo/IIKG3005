3ffc3f8f2c20c7fd7b17607d3f3e35ac2d4566c2
Kristoffer Juelsen <kristoju@stud.ntnu.no>
2023-09-04 17:54:10 +0200

    start of docker stuff

---
+++ WAAS/allowed_webhooks.js
@@ -0,0 +1,7 @@
+[
+    {
+      "id": "77c500b2-0e0f-4785-afc7-f94ed529c897",
+      "url": "https://webhook.site/#!/baeb73b7-f0a6-4db5-94a0-9adc9e58877a",
+      "token": "frKPI6p5LxxpJa8tCvVr=u5NvU66EJCQdybPuEmzKNeyCDul2-zrOx05?LwIhL5N"
+    }
+  ]
