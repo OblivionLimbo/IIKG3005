5c555f94c665f89a45fe358e97f08b0d955ede38
Kristoffer Juelsen <kristoju@stud.ntnu.no>
2023-09-26 14:27:08 +0200

    Added more variables

--- Mandatory Assignments/Assignment01/kristoju-oppg1/main.tf
+++ Mandatory Assignments/Assignment01/kristoju-oppg1/main.tf
@@ -1,51 +1,59 @@
 terraform {
-    required_providers {
-        azurerm = {
-        source  = "hashicorp/azurerm"
-        version = "2.46.0"
-        }
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "2.46.0"
     }
+  }
 }
 
 provider "azurerm" {
-    features {}
+  features {}
 }
 
 module "keyvault" {
-    source = "./keyvault"
-    kv_rgname = var.kv_rgname
-    kv_location = var.kv_location
-    kv_base_name = var.kv_base_name
-    sa_access_key = module.StorageAccount.primary_access_key_output
-    sa_name = module.StorageAccount.storage_account_name_output
+  source        = "./keyvault"
+  kv_rgname     = var.kv_rgname
+  kv_location   = var.kv_location
+  kv_base_name  = var.kv_base_name
+  sa_access_key = module.StorageAccount.primary_access_key_output
+  sa_base_name  = var.sa_base_name
+  vm_username   = var.vm_username
+  vm_password   = var.vm_password
 }
 
 module "StorageAccount" {
-    source = "./storageaccount"
-    sa_rgname = var.sa_rgname
-    sa_location = var.sa_location
-    sa_base_name = var.sa_base_name
-    sa_container_name = var.sa_container_name
+  source            = "./storageaccount"
+  sa_rgname         = var.sa_rgname
+  sa_location       = var.sa_location
+  sa_base_name      = var.sa_base_name
+  sa_container_name = var.sa_container_name
 }
 
 module "Network" {
-    source = "./network"
-    vnet_rg_name = var.vnet_rg_name
-    vnet_rg_location = var.vnet_rg_location
-    vnet_name = var.vnet_name
-    nsg_name = var.nsg_name
-    subnet_name = var.subnet_name
+  source               = "./network"
+  vnet_rg_name         = var.vnet_rg_name
+  vnet_rg_location     = var.vnet_rg_location
+  vnet_name            = var.vnet_name
+  vnet_address_space   = var.vnet_address_space
+  vnet_dns_servers     = var.vnet_dns_servers
+  nsg_name             = var.nsg_name
+  subnet_name          = var.subnet_name
+  subnet_address_space = var.subnet_address_space
 }
 
 module "VirtualMachine" {
-    source = "./virtualmachine"
-    vm_name = var.vm_name
-    vm_rg_name = var.vm_rg_name
-    vm_rg_location = var.vm_rg_location
-    vm_nic_name = var.vm_nic_name
-    vm_subnet_id = module.Network.subnet_id_output
-    pip_name = var.pip_name
-    keyvault_secret_password = module.keyvault.keyvault_secret_password_output
-    keyvault_secret_username = module.keyvault.keyvault_secret_username_output
+  source                    = "./virtualmachine"
+  vm_name                   = var.vm_name
+  vm_rg_name                = var.vm_rg_name
+  vm_rg_location            = var.vm_rg_location
+  vm_nic_name               = var.vm_nic_name
+  vm_size                   = var.vm_size
+  vm_nic_private_ip_address = var.vm_nic_private_ip_address
+  vm_nic_public_ip_address  = var.vm_nic_public_ip_address
+  pip_name                  = var.pip_name
+  vm_username               = module.keyvault.keyvault_secret_password
+  vm_password               = module.keyvault.keyvault_secret_username
+  vm_subnet_id              = module.Network.subnet_id_output
 }
 
