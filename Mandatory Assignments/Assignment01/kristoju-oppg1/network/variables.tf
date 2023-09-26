e381c163e341e2b279b62f4f58e402c62aaa52f3
Kristoffer Juelsen <kristoju@stud.ntnu.no>
2023-09-26 14:28:24 +0200

    format and more variables

--- Mandatory Assignments/Assignment01/kristoju-oppg1/network/variables.tf
+++ Mandatory Assignments/Assignment01/kristoju-oppg1/network/variables.tf
@@ -1,24 +1,40 @@
 variable "vnet_rg_name" {
-    type = string
-    description = "Resource group name"
+  type        = string
+  description = "Resource group name"
 }
 
 variable "vnet_rg_location" {
-    type = string
-    description = "Resource group location"
+  type        = string
+  description = "Resource group location"
 }
 
 variable "vnet_name" {
-    type = string
-    description = "Virtual network name"
+  type        = string
+  description = "Virtual network name"
+}
+
+variable "vnet_address_space" {
+  type        = list(string)
+  description = "Address space for the virtual network"
+}
+
+variable "vnet_dns_servers" {
+  type        = list(string)
+  description = "DNS servers for the virtual network"
+}
+
+
+variable "subnet_name" {
+  type        = string
+  description = "Subnet name"
+}
+
+variable "subnet_address_space" {
+  type        = list(string)
+  description = "Address space for the subnet"
 }
 
 variable "nsg_name" {
-    type = string
-    description = "Network security group name"
-}
-
-variable "subnet_name" {
-    type = string
-    description = "Subnet name"
+  type        = string
+  description = "Network security group name"
 }
