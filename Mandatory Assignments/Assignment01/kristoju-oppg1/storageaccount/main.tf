ae3ea3581299949608ae9bf1ae25c85635ba6dd5
Kristoffer Juelsen <kristoju@stud.ntnu.no>
2023-09-28 20:04:07 +0200

    added sa variables

--- Mandatory Assignments/Assignment01/kristoju-oppg1/storageaccount/main.tf
+++ Mandatory Assignments/Assignment01/kristoju-oppg1/storageaccount/main.tf
@@ -16,8 +16,8 @@ resource "azurerm_storage_account" "sa" {
   name                     = "${lower(var.sa_base_name)}${random_string.random_string.result}"
   resource_group_name      = azurerm_resource_group.sa_rg.name
   location                 = azurerm_resource_group.sa_rg.location
-  account_tier             = "Standard"
-  account_replication_type = "GRS"
+  account_tier             = var.sa_account_tier
+  account_replication_type = var.sa_replication_type
 }
 
 resource "azurerm_storage_container" "storage_container" {
