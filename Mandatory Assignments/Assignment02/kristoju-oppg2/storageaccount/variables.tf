3404c42f028d136215d40e1e158e70116ccd63d8
Kristoffer Juelsen <kristoju@stud.ntnu.no>
2023-11-04 14:18:39 +0100

    added web page variables

--- Mandatory Assignments/Assignment02/kristoju-oppg2/storageaccount/variables.tf
+++ Mandatory Assignments/Assignment02/kristoju-oppg2/storageaccount/variables.tf
@@ -18,6 +18,16 @@ variable "sa_container_name" {
   description = "The name of the storage container"
 }
 
+variable "index_document" {
+  type        = string
+  description = "The name of the index document"
+}
+
+variable "source_content" {
+  type        = string
+  description = "The source content"
+}
+
 variable "common_tags" {
   description = "A map of common tags for resources."
   type        = map(string)
