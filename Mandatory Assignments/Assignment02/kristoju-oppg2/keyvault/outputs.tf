44a28bc9634fc169902c2f0a77244aa98790f1cc
Kristoffer Juelsen <kristoju@stud.ntnu.no>
2023-11-04 14:17:50 +0100

    added key vault outputs

--- Mandatory Assignments/Assignment02/kristoju-oppg2/keyvault/outputs.tf
+++ Mandatory Assignments/Assignment02/kristoju-oppg2/keyvault/outputs.tf
@@ -4,3 +4,15 @@ output "keyvault_name" {
 output "keyvault_id" {
   value = azurerm_key_vault.kv.id
 }
+
+output "keyvault_secret_username" {
+  value = azurerm_key_vault_secret.vm_username.value
+}
+
+output "keyvault_secret_password" {
+  value = azurerm_key_vault_secret.vm_password.value
+}
+
+output "keyvault_secret_sa_accesskey" {
+  value = azurerm_key_vault_secret.sa_accesskey.value
+}
