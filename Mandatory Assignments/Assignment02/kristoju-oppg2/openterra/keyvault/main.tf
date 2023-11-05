0590cebb5690c867587fbeb99260e9b0e97144a5
Kristoffer Juelsen <kristoju@stud.ntnu.no>
2023-11-05 14:43:01 +0100

    apply dev

--- Mandatory Assignments/Assignment02/kristoju-oppg2/openterra/keyvault/main.tf
+++ Mandatory Assignments/Assignment02/kristoju-oppg2/openterra/keyvault/main.tf
@@ -21,16 +21,10 @@ resource "random_password" "password" {
 
 data "azurerm_client_config" "current" {}
 
-resource "azurerm_resource_group" "kv_rg" {
-  name     = var.rg_name
-  location = var.rg_location
-  tags     = var.common_tags
-}
-
 resource "azurerm_key_vault" "kv" {
   name                        = "${var.kv_base_name}${random_string.random_string.result}"
-  location                    = azurerm_resource_group.kv_rg.location
-  resource_group_name         = azurerm_resource_group.kv_rg.name
+  location                    = var.rg_location
+  resource_group_name         = var.rg_name
   enabled_for_disk_encryption = true
   tenant_id                   = data.azurerm_client_config.current.tenant_id
   soft_delete_retention_days  = 7
