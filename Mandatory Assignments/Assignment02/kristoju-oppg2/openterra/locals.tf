8200efcc16721f870673ba55ae9ed1fccd63ab5e
Kristoffer Juelsen <kristoju@stud.ntnu.no>
2023-11-05 14:57:13 +0100

    apply dev

--- Mandatory Assignments/Assignment02/kristoju-oppg2/openterra/locals.tf
+++ Mandatory Assignments/Assignment02/kristoju-oppg2/openterra/locals.tf
@@ -1,9 +1,15 @@
+data "external" "current_user" {
+  program = ["bash", "-c", "az account show --query 'user.name' --output json"]
+}
+
 locals {
   common_tags = {
     company      = var.company
     project      = "${var.company}-${var.project}"
     billing_code = var.billing_code
     environment  = terraform.workspace == "default" ? "" : "${terraform.workspace}"
+    # Apply the current az user as the owner tag
+    owner = jsondecode(data.external.current_user.result)
   }
 
   workspaces_suffix = terraform.workspace == "default" ? "" : "${terraform.workspace}"
